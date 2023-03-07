b298fe0034807127d42d459b7005b66bdf91985d
Krzysztof Nofz <knofz@virtuslab.com>
2023-03-07 12:54:02 +0100

    feature/code_block changes styles for code block

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,9 @@ This is a demo for using Milkdown with **React**.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vulputate, augue vitae rutrum bibendum, lorem tellus vulputate dui, eget tristique mauris dui sit amet ipsum. Ut euismod, ex volutpat consequat gravida, quam dolor dapibus ex, eget aliquet leo magna posuere arcu. Praesent lacus diam, laoreet et nisi sit amet, interdum tristique enim. Morbi sapien tortor, suscipit in consectetur eu, finibus a turpis. Mauris scelerisque nisl quis purus pulvinar laoreet tincidunt ut nisl. Quisque ullamcorper turpis vel elit elementum, sit amet tempus tellus ultrices. Nullam ac lorem diam. Aenean aliquam pellentesque elit id tempor. Nullam diam lacus, vestibulum ac interdum vel, suscipit id lacus. Aliquam eget ante vitae dolor lacinia dignissim a sit amet tellus. Donec nec arcu eget purus molestie vulputate. Pellentesque pulvinar elementum posuere. Maecenas gravida, arcu ac sagittis aliquet, mauris risus sagittis augue, ac tristique turpis nulla id tellus. Nunc scelerisque ultrices odio, sed sodales neque pharetra ut. In faucibus sapien sed massa feugiat, eget pulvinar leo facilisis.
 
 \`\`\`typescript
-import { Button } from '../../common/Button';
-import { Icon } from '../../common/Icon/Icon';
-
-type ParagraphFormatProps = {
-  onActionClick: <T>(action: CmdKey<T>) => void;
-};
-
-export const ParagraphFormat: React.FC<ParagraphFormatProps> = ({
-  onActionClick,
-}) => (
-  <>
-    <li className="menubar-item menubar-item-paragraph_format">
-      <Button onClick={() => onActionClick(wrapInBulletListCommand.key)}>
-        <Icon icon="bulleted_list" />
-      </Button>
-    </li>
-    <li className="menubar-item">
-      <Button onClick={() => onActionClick(wrapInOrderedListCommand.key)}>
-        <Icon icon="numbered_list" />
-      </Button>
-    </li>
-  </>
-);
+function main() {
+  console.log('Hello milkdown!');
+}
 \`\`\`
 
 `;
