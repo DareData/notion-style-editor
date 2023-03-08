d87cf2a0c42d8043dc505b837c8d7a37baf8cdb7
Krzysztof Nofz <knofz@virtuslab.com>
2023-03-08 12:32:08 +0100

    feature/table removed commented code

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ const data = `# Milkdown React Commonmark
 
 This is a demo for using Milkdown with **React**.
 
+==very important words==
+
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vulputate, augue vitae rutrum bibendum, lorem tellus vulputate dui, eget tristique mauris dui sit amet ipsum. Ut euismod, ex volutpat consequat gravida, quam dolor dapibus ex, eget aliquet leo magna posuere arcu. Praesent lacus diam, laoreet et nisi sit amet, interdum tristique enim. Morbi sapien tortor, suscipit in consectetur eu, finibus a turpis. Mauris scelerisque nisl quis purus pulvinar laoreet tincidunt ut nisl. Quisque ullamcorper turpis vel elit elementum, sit amet tempus tellus ultrices. Nullam ac lorem diam. Aenean aliquam pellentesque elit id tempor. Nullam diam lacus, vestibulum ac interdum vel, suscipit id lacus. Aliquam eget ante vitae dolor lacinia dignissim a sit amet tellus. Donec nec arcu eget purus molestie vulputate. Pellentesque pulvinar elementum posuere. Maecenas gravida, arcu ac sagittis aliquet, mauris risus sagittis augue, ac tristique turpis nulla id tellus. Nunc scelerisque ultrices odio, sed sodales neque pharetra ut. In faucibus sapien sed massa feugiat, eget pulvinar leo facilisis.
 
 \`\`\`c
