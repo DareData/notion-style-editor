650b2ef0c4a6852265ff809530d57e8f12fd7d1d
Krzysztof Nofz <knofz@virtuslab.com>
2023-03-14 10:46:31 +0100

    Migrate to styled-components

--- src/lib/common/Button.tsx
+++ src/lib/common/Button.tsx
@@ -1,25 +1,68 @@
-import { clsx } from 'clsx';
 import { forwardRef } from 'react';
+import styled from 'styled-components';
+
+import { theme } from '../styles/theme';
+import { pxToRem } from '../styles/utils';
 
 type ButtonProp = 'primary' | 'secondary';
 
-type ButtonProps = React.HTMLProps<HTMLButtonElement> & {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   type?: 'button' | 'submit' | 'reset';
   prop?: ButtonProp;
+  oval?: boolean;
   children?: React.ReactNode;
 };
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { type = 'button', prop = 'primary', children, className = '', ...rest },
+    { type = 'button', prop = 'primary', oval = false, children, ...rest },
     buttonRef
   ) => (
-    <button
+    <ButtonStyled
       {...{ type }}
       {...rest}
       ref={buttonRef}
-      className={clsx('button', prop, className)}>
+      $oval={oval}
+      $prop={prop}>
       {children}
-    </button>
+    </ButtonStyled>
   )
 );
+
+const buttonColors = {
+  bg: {
+    hover: {
+      primary: theme.colors.secondaryGrey,
+      secondary: theme.colors.green,
+    },
+  },
+  border: {
+    hover: {
+      primary: theme.colors.green,
+      secondary: 'transparent',
+    },
+  },
+};
+
+const ButtonStyled = styled.button<{ $oval: boolean; $prop: ButtonProp }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${pxToRem(7)};
+  background-color: transparent;
+  border: ${pxToRem(1)} solid transparent;
+  outline: 0;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in, border-color 0.2s ease-in;
+  ${props =>
+    props.$oval &&
+    `
+    border-radius: ${pxToRem(8)};
+  `}
+
+  &:hover,
+  &:focus {
+    background-color: ${props => buttonColors.bg.hover[props.$prop]};
+    border-color: ${props => buttonColors.border.hover[props.$prop]};
+  }
+`;
--- src/lib/packages/TextEditor.tsx
+++ src/lib/packages/TextEditor.tsx
@@ -1,8 +1,10 @@
 import { MilkdownProvider, Milkdown as MilkdownEditor } from '@milkdown/react';
 import { ProsemirrorAdapterProvider } from '@prosemirror-adapter/react';
+import { ThemeProvider } from 'styled-components';
 
 import { EditorContextProvider } from '../components/EditorContext/EditorContextProvider';
 import { MenuBar } from '../components/MenuBar/MenuBar';
+import { theme } from '../styles/theme';
 
 export type TextEditorProps = {
   data: string;
@@ -13,16 +15,18 @@ export const TextEditor: React.FC<TextEditorProps> = ({
   data,
   onDataChange,
 }) => (
-  <MilkdownProvider>
-    <ProsemirrorAdapterProvider>
-      <EditorContextProvider
-        defaultMarkdownValue={data}
-        onChange={onDataChange}>
-        <div className="date-data_text-editor">
-          <MenuBar />
-          <MilkdownEditor />
-        </div>
-      </EditorContextProvider>
-    </ProsemirrorAdapterProvider>
-  </MilkdownProvider>
+  <ThemeProvider {...{ theme }}>
+    <MilkdownProvider>
+      <ProsemirrorAdapterProvider>
+        <EditorContextProvider
+          defaultMarkdownValue={data}
+          onChange={onDataChange}>
+          <div className="date-data_text-editor">
+            <MenuBar />
+            <MilkdownEditor />
+          </div>
+        </EditorContextProvider>
+      </ProsemirrorAdapterProvider>
+    </MilkdownProvider>
+  </ThemeProvider>
 );
