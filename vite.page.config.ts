e6e6d8f1b4c29def96ae4884771fc4ff03484677
Krzysztof Nofz <knofz@virtuslab.com>
2023-03-23 14:28:20 +0100

    generate github pages

---
+++ vite.page.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              ssr: false,
+              pure: true,
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+    svgr(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'docs',
+      },
+    },
+  },
+});
