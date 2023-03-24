302291b12df4d406069800679f07f3777accbc85
Krzysztof Nofz <knofz@virtuslab.com>
2023-03-24 12:16:04 +0100

    bug/double-click-on-image fixed clicking on edit button

--- src/lib/hooks/useNotification.tsx
+++ src/lib/hooks/useNotification.tsx
@@ -2,8 +2,12 @@ import { toast } from 'react-hot-toast';
 
 export const useNotification = () => {
   const onErrorNotification = (message: string) => {
-    toast.error(message, { position: 'bottom-right' });
+    toast.error(message, { position: 'bottom-center' });
   };
 
-  return { onErrorNotification };
+  const onSuccessNotification = (message: string) => {
+    toast.success(message, { position: 'bottom-center' });
+  };
+
+  return { onErrorNotification, onSuccessNotification };
 };
