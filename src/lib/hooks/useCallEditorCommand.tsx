87d51f60e66a319e1ba995332809c5dcb62f3a02
Krzysztof Nofz <knofz@virtuslab.com>
2023-03-24 10:20:22 +0100

    feature/slash added slash commands

---
+++ src/lib/hooks/useCallEditorCommand.tsx
@@ -0,0 +1,21 @@
+import { CmdKey } from '@milkdown/core';
+import { useInstance } from '@milkdown/react';
+import { callCommand } from '@milkdown/utils';
+import { useCallback } from 'react';
+
+export const useCallEditorCommand = () => {
+  const [loading, getEditor] = useInstance();
+
+  const onCallCommand = useCallback(
+    <T,>(command: CmdKey<T>, payload?: T | undefined) => {
+      const editor = getEditor();
+      if (loading || !editor) {
+        return;
+      }
+      editor.action(callCommand(command, payload));
+    },
+    [loading, getEditor]
+  );
+
+  return { onCallCommand };
+};
