2237e517514453ea4b428ce06716867ece06e298
Krzysztof Nofz <knofz@virtuslab.com>
2023-03-28 17:58:41 +0200

    feature/google-slides fixed insert math command

--- src/lib/hooks/useInsertMathAction.tsx
+++ src/lib/hooks/useInsertMathAction.tsx
@@ -17,10 +17,12 @@ export const useInsertMathBlock = () => {
     }
 
     editor.action(ctx => {
-      const view = ctx.get(editorViewCtx);
-      const { state } = view;
-      const value = state.doc.content.firstChild?.textContent || '';
-      onCallCommand(insertMathCommand.key, value);
+      // const view = ctx.get(editorViewCtx);
+      // const { state } = view;
+      // const { selection } = state;
+      // const { from, to } = selection;
+      // const value = state.tr.doc.textBetween(from, to) || '';
+      onCallCommand(insertMathCommand.key);
     });
   }, [loading, getEditor, onCallCommand]);
 
