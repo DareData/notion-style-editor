e6e6022c6d10b63ad488bca805e7ef026e75f596
Krzysztof Nofz <knofz@virtuslab.com>
2023-04-03 11:16:59 +0200

    bug/link-is-not-clickable link is clickable

--- src/lib/packages/EditorContext/hooks/useCommonmarkPlugin.tsx
+++ src/lib/packages/EditorContext/hooks/useCommonmarkPlugin.tsx
@@ -46,7 +46,7 @@ export const useCommonmarkPlugin = () => {
           nodeViewFactory({
             component: LinkNode,
             as: 'span',
-            contentAs: 'a',
+            contentAs: 'span',
           })
         ),
       ].flat(),
