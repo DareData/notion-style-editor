e70523790f2ce6ea350c41468d516e966fe8245d
Krzysztof Nofz <knofz@virtuslab.com>
2023-04-04 11:10:12 +0200

    feature/added-typings added types to the package.json && changed lib name

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.tsx'),
-      name: 'milkdown-datedata',
+      name: 'altos-react-text-editor',
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
