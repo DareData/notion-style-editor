501aba97aa2024865cd28c05bf2e64efb5d68c72
Krzysztof Nofz <krzysztofnofz@gmail.com>
2024-04-30 10:43:49 +0200

    Release/0.3.2 (#94)
    * Feature/scihub 963 add GitHub workflows (#80)
    
    ---------
    
    Co-authored-by: Krzysztof Nofz <knofz@virtuslab.com>
    
    * Feature/scihub 1058 increase modal index (#84)
    
    ---------
    
    Co-authored-by: Krzysztof Nofz <knofz@virtuslab.com>
    
    * Feature: scihub-1209 google slide modal (#87)
    
    * feature/SCIHUB-1209: fixed typo in hook name
    
    * feature/SCIHUB-1209 add component prop
    
    * feature/SCIHUB-1209: fixed naming
    
    * feature/SCIHUB-1209: increment version
    
    * feature/SCIHUB-1209: whole modal as prop
    
    ---------
    
    Co-authored-by: Krzysztof Nofz <knofz@virtuslab.com>
    
    * Feature/scihub 1247 enable mentioning users (#89)
    
    * feature/SCIHUB-1247 enable mentioning users
    
    ---------
    
    Co-authored-by: Krzysztof Nofz <knofz@virtuslab.com>
    
    * Bug/scihub 1401 edit mode does not change (#91)
    
    * bug/SCIHUB-1401 migrate to new milkdown way of handling editable flag
    
    * bug/SCIHUB-1401 updated changelog
    
    * bug/SCIHUB-1401 remove type from useRef
    
    ---------
    
    Co-authored-by: Krzysztof Nofz <knofz@virtuslab.com>
    
    * Feature/kap 510 add is dirty (#93)
    
    * feature/KAP-510 add is dirty
    
    * feature/KAP-510 updated changelog
    
    ---------
    
    Co-authored-by: Krzysztof Nofz <knofz@virtuslab.com>
    
    * release/0.3.2 updated version
    
    ---------
    
    Co-authored-by: Krzysztof Nofz <knofz@virtuslab.com>
    Co-authored-by: Mateusz Kleszcz <mateusz.kacper.kleszcz@gmail.com>

--- src/lib/packages/Editor.tsx
+++ src/lib/packages/Editor.tsx
@@ -1,44 +1,59 @@
 import { EditorStatus, editorViewCtx, serializerCtx } from '@milkdown/core';
+import type { Editor as MilkdownEditorType } from '@milkdown/core';
 import { Milkdown as MilkdownEditor, useInstance } from '@milkdown/react';
 import { forwardRef, useImperativeHandle } from 'react';
+
+const getValue = (editor: MilkdownEditorType | undefined, loading: boolean) => {
+  if (loading || !editor || editor.status !== EditorStatus.Created) {
+    return undefined;
+  }
+  return editor.action(ctx => {
+    const view = ctx.get(editorViewCtx);
+    const serializer = ctx.get(serializerCtx);
+    const { state } = view;
+
+    return serializer(state.tr.doc);
+  });
+};
 
 export type EditorRef = {
   reset: () => void;
   getValue: () => string | undefined;
+  isEditorDirty: () => boolean;
 };
 
-type EditorProps = {};
+type EditorProps = {
+  defaultValue: string;
+};
 
-export const Editor = forwardRef<EditorRef, EditorProps>((props, ref) => {
-  const [loading, getEditor] = useInstance();
+export const Editor = forwardRef<EditorRef, EditorProps>(
+  ({ defaultValue }, ref) => {
+    const [loading, getEditor] = useInstance();
 
-  useImperativeHandle(ref, () => ({
-    reset: () => {
-      const editor = getEditor();
-      if (loading || !editor || editor.status !== EditorStatus.Created) {
-        return;
-      }
-      editor.action(ctx => {
-        const view = ctx.get(editorViewCtx);
-        const { state } = view;
+    useImperativeHandle(ref, () => ({
+      reset: () => {
+        const editor = getEditor();
+        if (loading || !editor || editor.status !== EditorStatus.Created) {
+          return;
+        }
+        editor.action(ctx => {
+          const view = ctx.get(editorViewCtx);
+          const { state } = view;
 
-        view.dispatch(state.tr.replace(0, state.doc.content.size));
-      });
-    },
-    getValue: () => {
-      const editor = getEditor();
-      if (loading || !editor || editor.status !== EditorStatus.Created) {
-        return undefined;
-      }
-      return editor.action(ctx => {
-        const view = ctx.get(editorViewCtx);
-        const serializer = ctx.get(serializerCtx);
-        const { state } = view;
+          view.dispatch(state.tr.replace(0, state.doc.content.size));
+        });
+      },
+      getValue: () => {
+        const editor = getEditor();
+        return getValue(editor, loading);
+      },
+      isEditorDirty: () => {
+        const editor = getEditor();
+        const value = getValue(editor, loading);
+        return value !== defaultValue;
+      },
+    }));
 
-        return serializer(state.tr.doc);
-      });
-    },
-  }));
-
-  return <MilkdownEditor />;
-});
+    return <MilkdownEditor />;
+  }
+);
